b5011adb2df2a2924e21471820c89826bb0c771e
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-04-10 23:16:55 +0600

    basic commit

---
+++ js_problem_3.js
@@ -0,0 +1,72 @@
+/*
+Add two numbers: 12 and 8
+Subtract 45 from 90
+Multiply 9 and 7
+Divide 81 by 9
+*/
+
+let ax = 12;
+let bx = 8;
+console.log(ax + bx);
+
+let mx = 90;
+let nx = 45;
+console.log(mx - nx);
+
+console.log(9 * 7);
+console.log(81 / 9);
+
+/*
+Create two variables and perform all four operations (+, -, *, /)
+Find the total price of 5 items if each costs 120
+Calculate the average of 3 numbers
+*/
+
+let var1 = 25; let var2 = 5;
+console.log(var1 + var2, var1 - var2, var1 * var2, var1 / var2);
+
+let price = 120;
+console.log("total price of 5 items:", price * 5);
+
+let ave1 = 10; let ave2 = 20; let ave3 = 30;
+console.log("average of 3 numbers:",(ave1 + ave2 + ave3) / 3);
+
+/*
+🔹 Math.round()
+Level 1
+Round 5.4
+Round 8.6
+Level 2
+Round the value of a variable (e.g., 9.78)
+Round the result of division (e.g., 25 / 4)
+*/
+
+let numX = 5.4;
+console.log(Math.round(numX));  // 5
+console.log(Math.round(8.6));   // 9
+
+console.log(Math.round(9.78));  // 10
+console.log(Math.round(25 / 4));// 6
+
+/*
+🔹 Math.random()
+Level 1
+Generate a random number between 0 and 1
+Level 2
+Generate a random number between 1 and 10
+Generate a random number between 1 and 50
+Simulate a dice roll (1–6)
+*/
+
+console.log("random number between 0 and 1:",Math.random());
+console.log("random number between 1 and 10:",Math.floor(Math.random() * 10) + 1);
+console.log("random number between 1 and 50:",Math.floor(Math.random() * 50) + 1);
+
+/*
+🔹 Math.max()
+Level 1
+Find the maximum of 5, 12, 8
+Level 2
+Find the largest number among 4 variables
+Find the maximum value from an array
+*/
