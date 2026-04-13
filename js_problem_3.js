027434dc15489d138a0856dd432af8c20f31ffb3
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-04-13 10:20:05 +0600

    update basic commit

--- js_problem_3.js
+++ js_problem_3.js
@@ -107,3 +107,55 @@ Convert "75abc" into an integer
 console.log(parseInt("50"));
 console.log(parseInt("100px"));
 console.log(parseInt("75abc"));
+
+/*
+🔹 parseFloat()
+Level 1
+Convert "45.67" into a number
+Level 2
+Convert "99.99$" into a float
+Convert "123.456abc" into a float
+*/
+
+console.log(parseFloat("45. 67"));
+console.log("99.99$");
+console.log("123.456abc");
+
+/*
+🟡 Mixed / Medium Problems
+Generate a random number between 1–100 and round it
+Convert a string number to integer, then add 10
+Convert a string float to number and format it to 2 decimal places
+Find the largest number among three variables
+Create a simple calculator using +, -, *, /
+Generate a random number (1–10) and check if it is even or odd
+*/
+
+
+let rand = Math.random() * 100;
+console.log(Math.round(rand));
+
+let str = "30";
+console.log(parseInt("30") + 10);
+
+let fltStr = "58.746354";
+console.log(parseFloat(fltStr).toFixed(2));
+
+let a1 = 10, b1 = 20, c1 = 15;
+console.log(Math.max(a1, b1, c1));
+
+// calculator
+
+let input1 = 20; let input2 = 5;
+let operator = "/";
+
+if (operator === "+") console.log(input1 + input2);
+else if (operator === "-") console.log(input1 - input2);
+else if (operator === "*") console.log(input1 * input2);
+else if (operator === "/") console.log(input1 / input2);
+
+// Even or Odd
+
+let randomNum = Math.floor(Math.random() * 10) + 1;
+if (randomNum % 2 === 0) {console.log("Even");}
+else {console.log("Odd");}
