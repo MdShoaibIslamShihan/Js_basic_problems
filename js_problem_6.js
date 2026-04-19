e9406e5e536e09410c8b7f4ac7f9f9a46b0a4a6a
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-04-19 15:18:13 +0600

    basic commit

---
+++ js_problem_6.js
@@ -0,0 +1,11 @@
+/*
+1. Even or Odd
+Write a program that checks if a number is even or odd
+*/
+
+let check = 33;
+if (check % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
