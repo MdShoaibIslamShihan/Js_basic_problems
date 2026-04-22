bd3c26ecf3f72dbb4bdc9ebd285907cc5c60862f
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-04-22 12:35:55 +0600

    update basic commit

--- js_problem_7.js
+++ js_problem_7.js
@@ -29,3 +29,39 @@ function sumArray(arr) {
     console.log(total);
 }
 sumArray([1, 2, 3, 4, 5]);
+
+// Declare a function `flattenArray` that takes a nested array (any depth) and prints a fully flattened version without using Array.flat().
+// e.g., [1,[2,[3,[4]]]] → [1,2,3,4]
+
+function flattenArray(arr) {
+    let result = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flattenArray(arr[i]));
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+console.log(flattenArray([1,[2,[3,[4]]]]));
+
+// Write a function `add` that accepts two parameters `a` and `b` and prints their sum.
+//Hint: Both parameters are positional — order matters when calling.
+
+function add(a, b) {
+    console.log(a + b);
+}
+add(5, 5);
+
+/*
+Create a function `introduce` with parameters `name` and `age` that prints: 'Hi, I'm [name] and I'm [age] years old.'
+Hint: Use a template literal with backticks.
+*/
+
+function introduce(name, age) {
+    console.log(`Hi, I'm ${name} and I'm ${age} years old`);
+}
+introduce("Khamba", 43);
