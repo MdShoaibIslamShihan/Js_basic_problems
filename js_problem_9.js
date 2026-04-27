adc9642ebe2157b78ec32e10d7cc8fad9c11c1f7
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-04-27 22:32:38 +0600

    basic commit

---
+++ js_problem_9.js
@@ -0,0 +1,12 @@
+/*
+Create an object called student with properties: name, age, and grade.
+Print the name and grade
+*/
+
+let student = {
+    name: "DonKali",
+    age: 15,
+    grade: "A",
+}
+console.log(student.name);
+console.log(student.grade); 
