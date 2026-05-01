6e4194e57eff9003e5f679c8a00e49c26fd01c54
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-05-01 22:41:24 +0600

    update basic commit

--- js_problem_11.js
+++ js_problem_11.js
@@ -11,3 +11,38 @@ console.log(typeof 25);
 console.log(typeof true);
 console.log(typeof null);
 console.log(typeof undefined);
+
+/*
+Create Variables (Primitive Types)
+Create variables for:
+
+a string (your name)
+a number (your age)
+a boolean (are you a student)
+a null value
+an undefined variable
+Print all values
+*/
+
+let name = "Dimitry";
+let age = 39;
+let isStudent = true;
+let emptyValue = null;
+let notDefine;
+console.log(name, age, isStudent, emptyValue, notDefine);
+
+/*
+Array vs Object (Non-Primitive Basics)
+Create:
+an array of 3 colors
+an object with name and age
+Print both and identify which one is array and which one is object
+*/
+
+let colors = ["Red", "Green", "Blue"];
+let person = {
+    name: "Ali",
+    age: 10,
+}
+console.log(colors);
+console.log(person);
