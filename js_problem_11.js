06011aad2bf9083bb71b1ffc56fbe542d96d7753
Shoaib-Islam-Shihan <mdshoaibislamshihan@gmail.com>
2026-05-01 22:27:36 +0600

    basic commit

---
+++ js_problem_11.js
@@ -0,0 +1,13 @@
+/*
+Identify Primitive Types (Basics)
+Given the following values:
+"Hello", 25, true, null, undefined
+Identify the type of each value
+Print the type using typeof (where applicable)
+*/
+
+console.log(typeof "Hello");
+console.log(typeof 25);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
